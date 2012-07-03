d58c6cedfc668d1a5db9e82ec890b5bc22b8120d
Soumya Deb <debloper@gmail.com>
2012-07-03 19:12:24 +0530

    Pimpin' teh PHP response for cross-origin callback

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-$now = new DateTime;
-echo $now->format("Y-m-d H:i:s T");
+	$now = new DateTime;
+	$now = $now->format("Y-m-d H:i:s T");
+
+	if (array_key_exists('callback', $_GET)) {
+		header('Content-Type: text/javascript; charset=utf8');
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+		echo $_GET['callback'] . '(' . json_encode($now) . ');';
+	} else {
+		header('Content-Type: text/plain; charset=utf8');
+		echo $now;
+	}
 ?>
