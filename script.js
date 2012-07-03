ee7a3d3f4da026de8665f36703761f64467b066f
Soumya Deb <debloper@gmail.com>
2012-07-03 19:18:54 +0530

    Server-side request handling: OK

--- script.js
+++ script.js
@@ -16,14 +16,25 @@ var accordion = {};
 			headers[i].className = "collapsed";
 		}
 	}
-	accordion.handleAction =  function(event) {
+	accordion.setup = function(event) {
 		for (var i in headers) {
 			headers[i].className = "collapsed";
 		} // Let's set all contents to collapsed, better trade-off than if{}
 		event.target.className = "expanded";
+		accordion.fetch();
+	}
+	accordion.callback = function(data) {
+		alert(data);
+	}
+	accordion.fetch = function() {
+		var jsonp = document.createElement('script');
+		var remote = "http://gears.debs.io/challengeAccepted/"
+		,	callback = "?callback=accordion.callback";
+		jsonp.src = remote + callback;
+		document.getElementsByTagName('head')[0].appendChild(jsonp);
 	}
 
 	for (var j in headers) {
-		headers[j].addEventListener("click", accordion.handleAction);
+		headers[j].addEventListener("click", accordion.setup);
 	}
 })();
