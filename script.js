310d65d9b288072f469d333dffa536fe0a1c5a97
Soumya Deb <debloper@gmail.com>
2012-07-03 06:12:56 +0530

    Dynamic accordion setting

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Here goes the copyright/author info & stuff - I'm lazy now, sue me!
 
+var accordion = {};
+
 (function() {
 	var el = document.getElementById("accordion")
 	,	headers = el.querySelectorAll("h3")
 	,	contents = el.querySelectorAll("h3 + div");
 
-	var texts;
-	for (var i in contents) {
-		if (i == 0) continue; // Hard-coding display 1st one for now
-		texts = contents[i].querySelectorAll("*");
-		for (var j in texts) {
-			if (texts[j].style) {
-				texts[j].style.display = "none";
-			} // Cause white-spaces are screwing up DOM manipulation
-		} // I don't wanna directly apply "display: none" to the div
-		// because, I'd later try to use CSS-transition on its height
+	accordion.setAccordion = function(order) {
+		for (var i in contents) {
+			if (i == order) {
+				contents[i].className = "expanded";
+				continue;
+			}
+			contents[i].className = "collapsed";
+		}
 	}
 })();
