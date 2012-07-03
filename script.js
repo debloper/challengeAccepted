45f0676c35fb855aa1ab1748f2c25befa9e9deff
Soumya Deb <debloper@gmail.com>
2012-07-03 20:30:50 +0530

    Last minute clean-ups, comments & remove IE polyfil

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-// Here goes the copyright/author info & stuff - I'm lazy now, sue me!
+// Do What The Fuck You Want To Public License [WTFPL]: http://sam.zoy.org/wtfpl
+// Copyright (C) 2012 Soumya Deb <debloper@gmail.com>
 
-var accordion = {};
+var accordion = {};		/// The global object, contains exposable data & methods
 
 (function() {
 	var el = document.getElementById("accordion")
-	,	headers = el.querySelectorAll("h3");
+	,	headers = el.querySelectorAll("h3");	///// `el` could've been dynamic
 
-	accordion.target = headers[0];	// Default value for targer header
+	accordion.target = headers[0];			//// Default value for targer header
 
 	accordion.init = function(order) {
 		for (var i in headers) {
-			if (!order) order = 0;
+			if (!order) order = 0;			//// Default value for targer header
 			if (i == order) {
 				headers[i].className = "expanded";
-				continue;
+				continue;			//// Sets only the target header as expanded
 			}
-			headers[i].className = "collapsed";
+			headers[i].className = "collapsed";	/// Collapse rest of the headers
 		}
 	}
 	accordion.content = function(target) {
 		var content = target.nextSibling;
-		if (!content.style) { // Let's make it recursive, if whitespace node
+		if (!content.style) { 		//// Let's recurse it, to avoid white-spaces
 			content = accordion.content(content);
-		} // to tacle https://developer.mozilla.org/en/Whitespace_in_the_DOM
-		accordion.target = content
+		} 	//// To tacle https://developer.mozilla.org/en/Whitespace_in_the_DOM
+		accordion.target = content; /// Sets the target-content for the callback
 		return accordion.target;
 	}
 	accordion.setup = function(event) {
 		for (var i in headers) {
 			headers[i].className = "collapsed";
-		} // Let's set all contents to collapsed, better trade-off than if{}
-		event.target.className = "expanded";
-		accordion.target = accordion.content(event.target);
-		accordion.fetch();
+		} 		// Let's collapse all headers, better trade-off than nested if{}
+		event.target.className = "expanded";	// And, expand the target header
+		accordion.target = accordion.content(event.target); /// Get content-node
+		accordion.fetch();			// Initiate remote-server request over JSONP
 	}
 	accordion.callback = function(data) {
-		accordion.target.innerHTML = "<p>" + data + "</p>";
+		accordion.target.innerHTML = "<p>" + data + "</p>";	// Sets fetched data
 	}
 	accordion.fetch = function() {
 		var jsonp = document.createElement('script');
 		var remote = "http://gears.debs.io/challengeAccepted/"
 		,	callback = "?callback=accordion.callback";
-		jsonp.src = remote + callback;
-		document.getElementsByTagName('head')[0].appendChild(jsonp);
+		jsonp.src = remote + callback;			/// Rig the JavaScript for JSONP
+		document.getElementsByTagName('head')[0].appendChild(jsonp);	// FIRE!
 	}
 
 	for (var j in headers) {
 		headers[j].addEventListener("click", accordion.setup);
+		//// Adds event listener hooks to the headers to handle the click events
 	}
 })();
