41ea34b03016dd8544fd94dc352e9405a4b77753
Soumya Deb <debloper@gmail.com>
2012-07-03 06:49:54 +0530

    Draft working prototype

--- script.js
+++ script.js
@@ -16,4 +16,24 @@ var accordion = {};
 			contents[i].className = "collapsed";
 		}
 	}
+	accordion.getContent = function(header) {
+		var content = header.nextSibling;
+		if (!content.style) { // Let's make it recursive, if whitespace node
+			content = accordion.getContent(content);
+		} // to tacle https://developer.mozilla.org/en/Whitespace_in_the_DOM
+		return content;
+	}
+	accordion.handleAction =  function(event) {
+		for (var i in contents) {
+			contents[i].className = "collapsed";
+		} // Let's set all contents to collapsed, better trade-off than if{}
+		var content = accordion.getContent(event.target.parentNode);
+		// event.target == the anchor tag, so the parent is the h3
+		content.className = "expanded";
+		event.preventDefault();
+	}
+
+	for (var j in headers) {
+		headers[j].addEventListener("click", accordion.handleAction);
+	}
 })();
